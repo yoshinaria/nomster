86714ea48624a77c1d38dd1aa3d610ec1f69c89b
Akiko <yoshinariakiko@gmail.com>
2015-05-15 02:03:54 +0000

    added Geocoder

--- Gemfile
+++ Gemfile
@@ -35,6 +35,10 @@ gem 'simple_form'
 
 gem 'devise', '>= 3.2.4'
 
+gem 'geocoder'
+
+gem 'figaro', '>= 1.0.0'
+
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
   gem 'sdoc', require: false
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -58,6 +58,6 @@ class PlacesController < ApplicationController
   private
 
   def place_params
-    params.require(:place).permit(:name, :description, :address)
+    params.require(:place).permit(:name, :address, :description)
   end
 end
